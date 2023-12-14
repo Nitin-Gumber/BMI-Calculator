93a63b5b3b66664a80bd0294c670167186904b6e
Nitin Gumber <aroranitin486@gmail.com>
2023-12-14 13:39:45 +0530

    Change weight Scale in feet and inches

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
 const form = document.querySelector("form");
 const bmiRange = document.querySelector("#bmiRange");
 
-// This usecase will give you empty
-// const height = parseInt(documen.querySelector('#height').value)
-
 form.addEventListener("submit", function (e) {
   e.preventDefault();
 
-  const height = parseInt(document.querySelector("#height").value);
-  const weight = parseInt(document.querySelector("#weight").value);
+  const height = parseFloat(document.querySelector("#height").value);
+  const weightInKg = parseFloat(document.querySelector("#weight").value);
   const results = document.querySelector("#results");
   if (height === "" || height < 0 || isNaN(height)) {
     results.innerHTML = `Please give a valid height ${height}`;
-  } else if (weight === "" || weight < 0 || isNaN(weight)) {
-    results.innerHTML = `Please give a valid weight ${weight}`;
+  } else if (weightInKg === "" || weightInKg < 0 || isNaN(weightInKg)) {
+    results.innerHTML = `Please give a valid weight ${weightInKg}`;
   } else {
-    results.innerHTML = (weight / ((height * height) / 10000)).toFixed(2);
+    var heightInCM = height * 30.48;
+    results.innerHTML = (weightInKg / ((heightInCM * heightInCM) / 10000 )).toFixed(2);
   }
   if (results.innerHTML < 18.6) {
     bmiRange.innerHTML = `Under Weight`;

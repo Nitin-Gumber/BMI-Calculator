8cca1d86689473e83200772c95c1088373a57679
Nitin Gumber <aroranitin486@gmail.com>
2023-12-16 00:26:38 +0530

    Add Comments -- More readability & Change some code!

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
+// Selectors
 const form = document.querySelector("form");
 const bmiRange = document.querySelector("#bmiRange");
 
+// Event Listener
 form.addEventListener("submit", function (e) {
-  e.preventDefault();
-
   const height = parseFloat(document.querySelector("#height").value);
   const weightInKg = parseFloat(document.querySelector("#weight").value);
   const results = document.querySelector("#results");
+
+  // Condition for Height and Weight
   if (height === "" || height < 0 || isNaN(height)) {
     results.innerHTML = `Please give a valid height ${height}`;
   } else if (weightInKg === "" || weightInKg < 0 || isNaN(weightInKg)) {
     results.innerHTML = `Please give a valid weight ${weightInKg}`;
   } else {
-    var heightInCM = height * 30.48;
+    const heightInCM = height * 30.48;
     results.innerHTML = (weightInKg / ((heightInCM * heightInCM) / 10000 )).toFixed(2);
   }
+
+  // Condition for BMI Range
   if (results.innerHTML < 18.6) {
     bmiRange.innerHTML = `Under Weight`;
   }
